fdc7d5a7753a6573f41fd2243c0a2909933698bc
marylukashkina <marylukashkina@gmail.com>
2025-03-10 16:54:31 +0300

    project02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,31 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
+let dice1   // your code
 let dice2 // your code
-let isWinningDouble // your code
+let isWinningDouble = false // your code
+
+while (!isWinningDouble) {
+  dice1 = Math.floor(Math.random() * 6) +1
+  console.log('Первый бросок: ' + dice1);
+  
+  dice2 = Math.floor(Math.random() * 6) +1
+  console.log('Второй бросок: ' + dice2);
+
+ if (dice2 > 3 && dice1 === dice2) {
+  isWinningDouble = true
+  console.log('Выигрышный дубль!');
+  
+ }else{
+  console.log('Не выигрышный дубль.');
+ }
+}
+
+
+
+
+
+
 
 /*
 console.log('Первый бросок: ' + dice1)
