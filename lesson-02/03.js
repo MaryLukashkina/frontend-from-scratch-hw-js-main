0abe20787c5c788bd5fe5bea99b3433a9e7f4fe4
marylukashkina <marylukashkina@gmail.com>
2025-03-18 14:38:19 +0300

    project01_01

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,21 +4,42 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 80 // тестовое значение, можно изменять (балл)
+const score = 36 // тестовое значение, можно изменять (балл)
 let grade     //оценка студента
 // your code
 
+switch (true) {
+    case score >= 0 && score <= 49:
+        grade = "F"
+        break;
+    case score >= 50 && score <= 69:
+        grade = "D"
+        break;
+    case score >= 70 && score <= 79:
+        grade = "C"
+        break;
+    case score >= 80 && score <= 89:
+        grade = "B"
+        break;
+    case score >= 90 && score <= 100:
+        grade = "A"
+        break;
+} console.log(grade);
 
-if (score>=0 && score<=49) {
-    grade = "F"
-} else if (score>=50 && score<=69){
-    grade = "D"
-} else if (score>=70 && score<=79){
-    grade = "C"
-} else if (score>=80 && score<=89){
-    grade = "B"
-} else if (score>=90 && score<=100){
-    grade = "A"
-}
 
-console.log(grade);
+
+
+
+// if (score>=0 && score<=49) {
+//     grade = "F"
+// } else if (score>=50 && score<=69){
+//     grade = "D"
+// } else if (score>=70 && score<=79){
+//     grade = "C"
+// } else if (score>=80 && score<=89){
+//     grade = "B"
+// } else if (score>=90 && score<=100){
+//     grade = "A"
+// }
+
+// console.log(grade);
--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,15 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+let number_1 = 5
+let number_2 = 3
+let sum = 0
+
+
+function calculateAverage(число1, число2, среднееЗначение) {
+    let number = number_1+number_2
+    sum = number/2
+    
+    console.log(sum);
+}
+calculateAverage(number_1, number_2, sum)
+
