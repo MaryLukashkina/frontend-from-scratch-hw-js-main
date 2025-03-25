7b5cea73e1bbd2f45730f6268e828c6a26490e47
marylukashkina <marylukashkina@gmail.com>
2025-03-25 17:40:46 +0300

    bdh04

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -39,11 +39,11 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 let array1 = [1, 2, 3];
 let array2 = [2, 3, 4];
 
-function findCommonElements(arr, arr1) {
+function findCommonElements(arr1, arr2) {
         let result = []                          
-        for (let i = 0; i < arr.length; i++) {
-            if (array2.includes(array1[i])) {
-                result.push(arr[i]);
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr2.includes(arr1[i])) {
+                result.push(arr1[i]);
             }
         }
         return result
--- lesson-04/04.js
+++ lesson-04/04.js
@@ -1,5 +1,6 @@
 /*
-Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый символ из исходной строки повторяется дважды.
+Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый 
+символ из исходной строки повторяется дважды.
 
 Входные данные:
 
@@ -12,4 +13,15 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+let str = "hello"
+
+function doubleEachCharacter(string){
+    let res = ""
+    for (let char of string){  //char это буква из строки(можно любое слово написать)
+        res = res + char + char
+        
+    }
+    return res
+} 
+
+console.log(doubleEachCharacter(str));
