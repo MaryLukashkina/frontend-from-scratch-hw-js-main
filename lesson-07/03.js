ad81b5790d20eeac59576d34828e4cb01a3b5f6e
marylukashkina <marylukashkina@gmail.com>
2025-04-14 16:59:13 +0300

    project03

--- lesson-07/03.js
+++ lesson-07/03.js
@@ -12,7 +12,7 @@
 
 function truncate(str, maxLength) {
   if (str.length > maxLength) {
-    return str.slice(0, maxLength - 3) + '...';
+    return str.slice(0, maxLength) + '...';
   }
   return str;
 }
